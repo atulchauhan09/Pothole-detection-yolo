9cfd5395f9171276a9cf2df66e72c584f963a9d8
Atul Chauhan <atulchauhanm22@gmail.com>
2026-04-03 13:35:14 +0530

    final fix model loading

--- app.py
+++ app.py
@@ -113,35 +113,72 @@
 
 #         result_img = results[0].plot()
 #         st.image(result_img, caption="Detection Result")
+# import streamlit as st
+# from PIL import Image
+# import numpy as np
+# import os
+# import gdown
+
+# # st.title("Pothole Detection System")
+
+# model_path = "best.pt"
+
+# # ✅ Download model if not present
+# if os.path.exists(model_path):
+#     os.remove(model_path)
+#     url = "https://drive.google.com/uc?id=1Xclx_BRdkR32q2wdlHaT9adbuyIllrqr"
+#     gdown.download(url, model_path, quiet=False)
+
+
+# # ✅ Load model safely (fix cv2 crash)
+# @st.cache_resource
+# def load_model():
+#     import os
+
+#     os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"  # safety fix
+#     from ultralytics import YOLO
+
+#     return YOLO(model_path)
+#     # from ultralytics import YOLO
+
+#     # return YOLO(model_path)
+
+
+# model = load_model()
+
+# uploaded_file = st.file_uploader("Upload Image")
+
+# if uploaded_file:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption="Uploaded Image")
+
+#     if st.button("Detect"):
+#         image_np = np.array(image)
+#         results = model(image_np, conf=0.2)
+
+#         result_img = results[0].plot()
+#         st.image(result_img, caption="Detection Result")
 import streamlit as st
 from PIL import Image
 import numpy as np
 import os
 import gdown
 
-# st.title("Pothole Detection System")
+st.title("Pothole Detection System")
 
-model_path = "best.pt"
-
-# ✅ Download model if not present
-if os.path.exists(model_path):
-    os.remove(model_path)
-    url = "https://drive.google.com/uc?id=1Xclx_BRdkR32q2wdlHaT9adbuyIllrqr"
-    gdown.download(url, model_path, quiet=False)
+MODEL_PATH = "best.pt"
 
 
-# ✅ Load model safely (fix cv2 crash)
 @st.cache_resource
 def load_model():
-    import os
+    # ✅ Download ONLY if not exists
+    if not os.path.exists(MODEL_PATH):
+        url = "https://drive.google.com/uc?export=download&id=1Xclx_BRdkR32q2wdlHaT9adbuyIllrqr"
+        gdown.download(url, MODEL_PATH, quiet=False)
 
-    os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"  # safety fix
     from ultralytics import YOLO
 
-    return YOLO(model_path)
-    # from ultralytics import YOLO
-
-    # return YOLO(model_path)
+    return YOLO(MODEL_PATH)
 
 
 model = load_model()
